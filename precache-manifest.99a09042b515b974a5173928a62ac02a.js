dea0812b530f31526be38a57d28bffc660e98f79
YazanAlaiwah <runner@fv-az18.kfwht4ucemkubhvj2l4mmf2ipg.cx.internal.cloudapp.net>
2020-06-29 13:07:46 +0000

    Updates

---
+++ precache-manifest.99a09042b515b974a5173928a62ac02a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2805ace170a573b63e098932b5c51b9",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "3e9387cea4d8f3a9b726",
+    "url": "/resty/static/css/main.a6b489a8.chunk.css"
+  },
+  {
+    "revision": "4a0c2299c3f13ec05011",
+    "url": "/resty/static/js/2.3f753960.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.3f753960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e9387cea4d8f3a9b726",
+    "url": "/resty/static/js/main.201ce38c.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  }
+]);
