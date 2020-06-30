9c83ffd686d6dc27ad9ad5f443071967b1f05fad
YazanAlaiwah <yazanalaiwah@gmail.com>
2020-06-30 19:04:26 +0300

    updata readme

--- src/components/results/results.js
+++ src/components/results/results.js
@@ -1,28 +1,22 @@
 import React from 'react';
 // import './form.scss';
-class Results extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="continar">
-        {this.props.count > 0 && (
-          <pre>
-            {JSON.stringify(
-              {
-                headers: this.props.headers,
-                count: this.props.count,
-                results: this.props.results,
-              },
-              null,
-              2
-            )}
-          </pre>
-        )}
-      </div>
-    );
-  }
+function Results(props) {
+  return (
+    <div className="continar">
+      {props.count > 0 && (
+        <pre>
+          {JSON.stringify(
+            {
+              headers: props.headers,
+              count: props.count,
+              results: props.results,
+            },
+            null,
+            2
+          )}
+        </pre>
+      )}
+    </div>
+  );
 }
 export default Results;
